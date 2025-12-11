0b084688b5bf58963140dbc8597064b0c7413631
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 13:04:40 +0600

    Add files via upload

--- src/basic/basic40.js
+++ src/basic/basic40.js
@@ -4,9 +4,10 @@
 */
 
 export function find(arr, callback) {
-
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            return arr[i];
+        }
+    }
     return undefined;
 }
-
-
-
