4af43324954cf2266dc5e228a3d2dece2988f0d2
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:48:49 +0600

    Add files via upload

--- src/basic/basic19.js
+++ src/basic/basic19.js
@@ -3,7 +3,11 @@
 */
 
 export function sum_positive(arr) {
-	return 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
 }
-
-
