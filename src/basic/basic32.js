554486fa545b55f9d248f6170b6ddc8e78430132
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:57:22 +0600

    Add files via upload

--- src/basic/basic32.js
+++ src/basic/basic32.js
@@ -3,5 +3,18 @@
 */
 
 export function matrixMultiplication(A, B) {
-  return [[1]];
+  const rowsA = A.length;
+  const colsA = A[0].length;
+  const colsB = B[0].length;
+  const result = Array(rowsA)
+    .fill()
+    .map(() => Array(colsB).fill(0));
+  for (let i = 0; i < rowsA; i++) {
+    for (let j = 0; j < colsB; j++) {
+      for (let k = 0; k < colsA; k++) {
+        result[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return result;
 }
