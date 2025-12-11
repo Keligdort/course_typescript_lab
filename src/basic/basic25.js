13602625ff1f9c197a88b87c0c83632794220cbb
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:50:09 +0600

    Add files via upload

--- src/basic/basic25.js
+++ src/basic/basic25.js
@@ -2,6 +2,19 @@
 	Реализуйте функцию removeFirstOccurrence(arr, value), которая удаляет первое вхождение указанного элемента из массива
 */
 
-export function removeFirstOccurrence (arr, value) {
-  return arr
+export function removeFirstOccurrence(arr, value) {
+  let index = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      index = i;
+      break;
+    }
+  }
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== index) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
