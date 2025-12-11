b539236daabdf51afbca122331050280ae73f37d
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:46:46 +0600

    Add files via upload

--- src/basic/basic14.js
+++ src/basic/basic14.js
@@ -3,5 +3,12 @@
 */
 
 export function even_elements(arr) {
-    return [];
+  if (!Array.isArray(arr)) return [];
+  const evenArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenArr.push(arr[i]);
+    }
+  }
+  return evenArr;
 }
