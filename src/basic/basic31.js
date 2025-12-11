13c61c4f0c2d11e249abeaecce16a920aee3a96d
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:53:30 +0600

    Add files via upload

--- src/basic/basic31.js
+++ src/basic/basic31.js
@@ -3,5 +3,14 @@
 */
 
 export function transposeMatrix(matrix) {	
-	return [[]];
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const transposed = [];
+  for (let j = 0; j < cols; j++) {
+    transposed[j] = [];
+    for (let i = 0; i < rows; i++) {
+      transposed[j][i] = matrix[i][j];
+    }
+  }
+  return transposed;
 }
