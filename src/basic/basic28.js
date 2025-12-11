6f334a2823d588392f7d79b262e3d8fd10d065e7
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:50:55 +0600

    Add files via upload

--- src/basic/basic28.js
+++ src/basic/basic28.js
@@ -2,7 +2,13 @@
 	Реализуйте функцию removeDuplicates(arr), которая удаляет повторяющиеся элементы из массива с помощью методов indexOf и push.
 */
 
-export function removeDuplicates(arr) {
 
-	return arr;
+export function removeDuplicates(arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) === -1) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
