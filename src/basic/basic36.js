b3ff6f4466b2f99e7d0bccd6ebb0c325b8201419
Keligdort <115645832+Keligdort@users.noreply.github.com>
2025-12-11 12:58:02 +0600

    Add files via upload

--- src/basic/basic36.js
+++ src/basic/basic36.js
@@ -4,10 +4,11 @@
 */
 
 export function filter(arr, f) {
-
-	return arr;
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (f(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 }
-
-
-
-
